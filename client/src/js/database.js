d58982e9770a0dd40ef1e5f63f0d0d03564b41a1
CECurtiss <curtissclinton@gmail.com>
2023-02-08 21:41:32 -0600

    added htmlwebpackplugin, css loader, and babel loader

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,34 +1,34 @@
-import { openDB } from "idb";
+import { openDB } from 'idb';
 
 const initdb = async () =>
-  openDB("jate", 1, {
+  openDB('jate', 1, {
     upgrade(db) {
-      if (db.objectStoreNames.contains("jate")) {
-        console.log("jate database already exists");
+      if (db.objectStoreNames.contains('jate')) {
+        console.log('jate database already exists');
         return;
       }
-      db.createObjectStore("jate", { keyPath: "id", autoIncrement: true });
-      console.log("jate database created");
+      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
+      console.log('jate database created');
     },
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => {
-  console.log("PUT to update the database");
-  const jateDb = await openDB("jate", 1);
-  const tx = jateDb.transaction("jate", "readwrite");
-  const store = tx.objectStore("jate");
-  await store.add({ jateDb: content });
+export const putDb = async (id, value) => {
+  console.log('PUT to update the database');
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  return await store.put({ id: id, jateDb: value });
   // console.error('putDb not implemented');
-};
+}
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
-  console.log("GET all from the database");
-  const jateDb = await openDB("jate", 1);
-  const tx = jateDb.transaction("jate", "readwrite");
-  const store = tx.objectStore("jate");
-  return await store.getAll();
-  // console.error('getDb not implemented');
-};
+ console.log('GET all from the database');
+ const jateDb = await openDB('jate', 1);
+ const tx = jateDb.transaction('jate', 'readwrite');
+ const store = tx.objectStore('jate');
+ return await store.getAll();
+// console.error('getDb not implemented');
+}
 initdb();
